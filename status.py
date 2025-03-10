88066ec87627b5670aec5aa4095422cd25ed7c36
Armaangg <newholland3639@gmail.com>
2025-03-10 06:58:26 +0530

    Update status.py

--- status.py
+++ status.py
@@ -77,3 +77,15 @@ def home():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
+    from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+class MyHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/index.html'
+        return super().do_GET()
+
+server_address = ('', 8000)
+httpd = HTTPServer(server_address, MyHandler)
+print("Server running at http://localhost:8000")
+httpd.serve_forever()
